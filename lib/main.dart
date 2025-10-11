fbe8860920c0d418950957a566f3f0723abc1466
Alexander Sambale <Alexander.Sambale@samxela.eu>
2025-10-11 21:31:43 +0200

    feat: Add UI with 12 months to input income

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,7 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Wie viel krieg ich?',
       theme: ThemeData(
         // This is the theme of your application.
         //
@@ -30,93 +31,142 @@ class MyApp extends StatelessWidget {
         // tested with just a hot reload.
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: TwelveInputFields(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
+enum Monat {
+  januar,
+  februar,
+  maerz,
+  april,
+  mai,
+  juni,
+  juli,
+  august,
+  september,
+  oktober,
+  november,
+  dezember,
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+extension MonatExtension on Monat {
+  String get name {
+    switch (this) {
+      case Monat.januar:
+        return 'Januar';
+      case Monat.februar:
+        return 'Februar';
+      case Monat.maerz:
+        return 'März';
+      case Monat.april:
+        return 'April';
+      case Monat.mai:
+        return 'Mai';
+      case Monat.juni:
+        return 'Juni';
+      case Monat.juli:
+        return 'Juli';
+      case Monat.august:
+        return 'August';
+      case Monat.september:
+        return 'September';
+      case Monat.oktober:
+        return 'Oktober';
+      case Monat.november:
+        return 'November';
+      case Monat.dezember:
+        return 'Dezember';
+    }
+  }
 
-  void _incrementCounter() {
+  int get nummer => index + 1; // Januar = 1, Dezember = 12
+}
+
+class TwelveInputFields extends StatefulWidget {
+  const TwelveInputFields({super.key});
+
+  @override
+  _TwelveInputFieldsState createState() => _TwelveInputFieldsState();
+}
+
+class _TwelveInputFieldsState extends State<TwelveInputFields> {
+  final List<TextEditingController> _controllers = List.generate(
+    12,
+    (index) => TextEditingController(text: '0'),
+  );
+
+  @override
+  void dispose() {
+    for (var controller in _controllers) {
+      controller.dispose();
+    }
+    super.dispose();
+  }
+
+  void _copyValueDown(int index) {
+    if (index < _controllers.length - 1) {
+      setState(() {
+        _controllers[index + 1].text = _controllers[index].text;
+      });
+    }
+  }
+
+  void _clearField(int index) {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      _controllers[index].text = '0';
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+        title: Text(
+          'Trage dein jeweiliges Bruttoeinkommen in € in die Monate ein.',
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      body: SingleChildScrollView(
+        padding: EdgeInsets.all(16),
+        child: Column(
+          children: List.generate(12, (index) {
+            return Padding(
+              padding: const EdgeInsets.symmetric(vertical: 8.0),
+              child: Row(
+                children: [
+                  // Expanded TextField
+                  Expanded(
+                    child: TextField(
+                      keyboardType: TextInputType.number,
+                      inputFormatters: [FilteringTextInputFormatter.digitsOnly],
+                      controller: _controllers[index],
+                      decoration: InputDecoration(
+                        labelText: Monat.values[index].name,
+                        border: OutlineInputBorder(),
+                      ),
+                    ),
+                  ),
+                  SizedBox(width: 8),
+                  // Down arrow button (if not the last input)
+                  if (index < _controllers.length - 1)
+                    IconButton(
+                      icon: Icon(Icons.arrow_downward),
+                      tooltip: 'Copy to next field',
+                      onPressed: () => _copyValueDown(index),
+                    ),
+                  // Clear button
+                  IconButton(
+                    icon: Icon(Icons.clear),
+                    tooltip: 'Clear field',
+                    onPressed: () => _clearField(index),
+                  ),
+                ],
+              ),
+            );
+          }),
+        ),
+      ),
     );
   }
 }
